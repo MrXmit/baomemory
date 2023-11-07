bccddb0725f70a295caa21a4624495f7ae6b9e5c
MrXmit <tonixsmit@gmail.com>
2023-11-07 13:36:30 +0200

    add Model

--- main_app/models.py
+++ main_app/models.py
@@ -2,25 +2,12 @@ from django.db import models
 
 # Create your models here.
 from django.db import models
-from datetime import date
-from django.urls import reverse
 from django.contrib.auth.models import User
 
-MEALS = (
-  ('B', 'Breakfast'),
-  ('L', 'Lunch'),
-  ('D', 'Dinner')
-)
+class Memory(models.Model):
+  title = models.CharField(max_length=100)
+  details = models.TextField(max_length=250)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-# class Finch(models.Model):
-#   name = models.CharField(max_length=100)
-#   breed = models.CharField(max_length=100)
-#   description = models.TextField(max_length=250)
-#   age = models.IntegerField()
-#   toys = models.ManyToManyField(Toy)
-#   user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-#   def __str__(self):
-#     return self.name
-#   def fed_for_today(self):
-#     return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+  def __str__(self):
+    return self.title
